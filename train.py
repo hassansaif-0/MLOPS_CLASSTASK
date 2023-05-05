42c519e3bcae45fdc04412b1e08b630ceeb4f5e9
hassansaif-0 <hassansaifraja102@gmail.com>
2023-05-05 23:51:19 +0500

    new files

---
+++ train.py
@@ -0,0 +1,150 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import RandomUnderSampler
+import json
+# Models
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+from sklearn.metrics import precision_recall_fscore_support, confusion_matrix, classification_report, accuracy_score
+from sklearn.model_selection import GridSearchCV
+from scipy import stats
+
+# Other
+import warnings
+warnings.filterwarnings("ignore")
+
+
+original = pd.read_csv('dataset.csv')
+df=original.copy()
+
+nan_counts = df.isnull().sum()
+
+# filter the nan_counts Series to include only columns with non-zero NaN counts
+nan_counts_filtered = nan_counts[nan_counts > 0]
+missing_cols = df.isna().mean() * 100
+missing_cols = missing_cols[missing_cols > 0]
+
+
+
+# Check for missing values in the "H" attribute
+missing = df['H'].isna()
+
+# Prepare the data for Little's MCAR test
+observed = np.array([sum(~missing), sum(missing)])
+expected = np.array([len(df) * (1 - np.mean(missing)), len(df) * np.mean(missing)])
+
+# Conduct the test and calculate the p-value
+chi2, p_value = stats.chisquare(observed, f_exp=expected)
+missing = df['diameter'].isna()
+
+# Prepare the data for Little's MCAR test
+observed = np.array([sum(~missing), sum(missing)])
+expected = np.array([len(df) * (1 - np.mean(missing)), len(df) * np.mean(missing)])
+
+# Conduct the test and calculate the p-value
+chi2, p_value = stats.chisquare(observed, f_exp=expected)
+missing = df['albedo'].isna()
+
+# Prepare the data for Little's MCAR test
+observed = np.array([sum(~missing), sum(missing)])
+expected = np.array([len(df) * (1 - np.mean(missing)), len(df) * np.mean(missing)])
+
+# Conduct the test and calculate the p-value
+chi2, p_value = stats.chisquare(observed, f_exp=expected)
+missing = df['diameter_sigma'].isna()
+
+# Prepare the data for Little's MCAR test
+observed = np.array([sum(~missing), sum(missing)])
+expected = np.array([len(df) * (1 - np.mean(missing)), len(df) * np.mean(missing)])
+
+# Conduct the test and calculate the p-value
+chi2, p_value = stats.chisquare(observed, f_exp=expected)
+# Remove the column which will not facilitate the analysis
+no_null_data=df.drop(['pdes', 'name', 'prefix', 'diameter', 'albedo', 'diameter_sigma'], axis=1)
+
+# Remove the row that includes null value
+no_null_data=no_null_data.dropna().reset_index(drop=True)
+no_null_data['spkid'] = no_null_data['spkid'].astype(str)
+from sklearn.calibration import LabelEncoder
+
+# Remove identifying columns
+data=no_null_data.drop(['id', 'spkid', 'full_name', 'orbit_id', 'equinox'], axis=1).reset_index(drop=True)
+
+# Encode categorical features and target
+one_hot_encoded_data = pd.get_dummies(data, columns=['neo', 'class'])
+one_hot_encoded_data['pha'] = LabelEncoder(
+).fit_transform(one_hot_encoded_data['pha'])
+from sklearn.model_selection import train_test_split
+
+# Split train, validation, and test sets
+x = one_hot_encoded_data.drop('pha', axis=1)
+y = one_hot_encoded_data['pha'].to_frame()
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.4, random_state=100, stratify=y)
+x_val, x_test, y_val, y_test = train_test_split(
+    x_test, y_test, test_size=0.5, random_state=100, stratify=y_test)
+from sklearn.preprocessing import StandardScaler
+
+# Normalizing the features
+# Normalizing after splitting could prevent leaking information about the validation set into the train set
+# StandardScaler() is useful in classification and Normalizer() is useful in regression
+x_train = StandardScaler().fit_transform(x_train)
+x_val = StandardScaler().fit_transform(x_val)
+x_test = StandardScaler().fit_transform(x_test)
+y_train.value_counts()
+from imblearn.over_sampling import SMOTE
+
+# Data Upsampling - SMOTE
+x_train_us, y_train_us = SMOTE(
+    sampling_strategy=0.5, random_state=100).fit_resample(x_train, y_train)
+y_train_us.value_counts()
+
+from imblearn.under_sampling import RandomUnderSampler
+
+# Data Undersampling - Random Undersampling
+random_under_sampling = RandomUnderSampler(random_state=100)
+x_train_us_rus, y_train_us_rus = random_under_sampling.fit_resample(x_train_us, y_train_us)
+
+y_train_us_rus.value_counts()
+
+from imblearn.over_sampling import SMOTE
+
+# Data Upsampling - SMOTE
+x_train_SMOTE, y_train_SMOTE = SMOTE(
+    sampling_strategy=0.5, random_state=100).fit_resample(x_train, y_train)
+y_train_SMOTE.value_counts()
+
+# Data Undersampling - Random Undersampling
+random_under_SAMPLING = RandomUnderSampler(random_state=100)
+x_train_us_UNDER, y_train_us_UNDER = random_under_SAMPLING.fit_resample(x_train_SMOTE, y_train_SMOTE)
+
+
+y_train_us_UNDER['pha'] = y_train_us_UNDER['pha'].map({0: 'N', 1: 'Y'})
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_score, recall_score, f1_score, classification_report
+
+
+rfc = RandomForestClassifier(class_weight='balanced', random_state=100)
+# Skip Hyperparameter Tuning part because parameter with dafult value get the highest accuracy of model
+
+rfc.fit(x_train_us_rus, y_train_us_rus)
+
+# Predict for validation set
+y_val_pred = rfc.predict(x_val)
+
+# Metrics
+precision_rfc, recall_rfc, fscore_rfc, support_rfc = precision_recall_fscore_support(
+    y_val, y_val_pred, average='macro')
+metrics = {'precision_rfc': precision_rfc, 'recall_rfc': recall_rfc}
+with open('metrics.json', 'w') as f:
+    json.dump(metrics, f)
